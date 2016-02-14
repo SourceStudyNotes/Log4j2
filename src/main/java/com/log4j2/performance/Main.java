780a544d3091f3b01b099d4ba1c8796203bd687c
wangbin01 <wangbin01@renrendai.com>
2016-02-14 10:20:32 +0800

    1.修改配置文件

--- src/main/java/com/log4j2/performance/Main.java
+++ src/main/java/com/log4j2/performance/Main.java
@@ -1,8 +1,5 @@
 package com.log4j2.performance;
 
-import java.text.MessageFormat;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,67 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
         Logger logger = threadLocalLogger.get();
-        System.out.println(logger + "|" + Thread.currentThread().getId());
         logger.info("Start");
-        Thread t1 = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                long start = System.currentTimeMillis();
-                for (int i = 0; i < 1; i++) {
-                    Logger logger = threadLocalLogger.get();
-                    System.out.println(logger.hashCode() + "|" + Thread.currentThread().getId());
-                    logger.info(MessageFormat.format("Performance metric:{0}", 10000));
-                }
-                long end = System.currentTimeMillis();
-                System.out.println("T1:" + (end - start));
-
-            }
-        }) {
-        };
-        Thread t2 = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                long start = System.currentTimeMillis();
-                Logger logger = threadLocalLogger.get();
-                System.out.println(logger.hashCode() + "|" + Thread.currentThread().getId());
-                for (int i = 0; i < 1; i++) {
-                    logger.info(MessageFormat.format("Performance metric:{0}", 10000));
-                }
-                long end = System.currentTimeMillis();
-                System.out.println("T2:" + (end - start));
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }) {
-        };
-        Thread t3 = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                long start = System.currentTimeMillis();
-                Logger logger = threadLocalLogger.get();
-                System.out.println(logger.hashCode() + "|" + Thread.currentThread().getId());
-                for (int i = 0; i < 1; i++) {
-                    logger.info(MessageFormat.format("Performance metric:{0}", 10000));
-                }
-                long end = System.currentTimeMillis();
-                System.out.println("T3:" + (end - start));
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }) {
-        };
-        t1.start();
-        t2.start();
-        t3.start();
     }
 
 }
